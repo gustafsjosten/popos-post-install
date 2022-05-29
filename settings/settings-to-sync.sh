cd64ea1cf4c85a530b8a736ab255bf128f61a651
Gustaf <gussjo@pop-os.localdomain>
2022-05-29 21:32:28 +0200

    Adding more

---
+++ settings/settings-to-sync.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# .zshrc
+# .profile
+# .oh-my-zsh
+# .condarc
+# .vscode (rather do with synced settings if possible)
+# .guake
+# gTile (find out where this is)
---
+++ sync/sync-settings.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+cp ~/.zshrc ~/data-for-post-installation/
+cp -r ~/.oh-my-zsh ~/data-for-post-installation/
+cp -r ~/.vscode  ~/data-for-post-installation/
